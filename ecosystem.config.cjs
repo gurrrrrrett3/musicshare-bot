a7cb8d4452cf3723ccd9bf74eb09ec3ae84b01a7
Garrett Keneau <gart@gart.sh>
2024-03-22 22:58:09 -0400

    esm fix 2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    apps : [{
+      name   : "musicshare",
+      script : "./dist/core/index.js",
+      args: [
+        "--color"
+      ]
+    }]
+  }
+  
--- ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-export const apps = [{
-    name: "musicshare",
-    script: "./dist/core/index.js",
-    args: [
-        "--color"
-    ]
-}];
-  
