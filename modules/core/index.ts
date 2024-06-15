bad50c63dd77736fd0e70000e0954972801d5949
Garrett Keneau <gart@gart.sh>
2024-06-15 01:23:12 -0400

    add status

--- modules/core/index.ts
+++ modules/core/index.ts
@@ -1,12 +1,35 @@
 import { bot } from "../../core/index.js";
 import Module from "../../core/base/module.js";
+import { ActivityType } from "discord.js";
 
 export default class CoreModule extends Module {
-name = "core";
-description = "The core commands for onebot";
+    name = "core";
+    description = "The core commands for onebot";
 
     getCoreModule(): CoreModule {
         return bot.moduleLoader.getModule("core") as CoreModule;
     }
 
+    async onLoad(): Promise<boolean> {
+
+        setInterval(() => {
+            this.updateStatus();
+        }, 300000); // 5 minutes
+
+        this.updateStatus();
+
+        return true;
+    }
+
+    public async updateStatus(): Promise<void> {
+
+        const guildCount = bot.client.guilds.cache.size;
+        const userCount = bot.client.users.cache.size;
+
+        bot.client.user!.setActivity({
+            type: ActivityType.Watching,
+            name: `${guildCount} servers | ${userCount} users`
+        });
+
+    }
 }
